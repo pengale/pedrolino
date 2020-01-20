f9ed5821b2e02b5ed67665e043be636c4ee8c51c
Pete Vander Giessen <pete.vandergiessen@canonical.com>
2020-01-20 22:06:57 +0200

    Stripped some cruft out of snapcraft.yaml, and moved from a oneshot daemon to an explictly focused script. I think this is now as minimal a reproducer as you can get!

--- reproducer.sh
+++ reproducer.sh
@@ -3,4 +3,6 @@
 set -ex
 
 snapcraft --debug
-sudo snap install --classic --dangerous pedrolino*.snap || echo "Bug probably found -- see journalctl for details."
+sudo snap install --classic --dangerous pedrolino*.snap
+pedrolino.test || \
+    echo "If the above output contains a DistributionNotFound exception, you have reproduced the bug!"
